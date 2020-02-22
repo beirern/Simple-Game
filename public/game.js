5caca701de37cf9e7b2e4707427367db42ae1017
Nicola Beirer <beirern@uw.edu>
2020-02-22 11:40:57 -0800

    moving functions to new file for easier reading. Trying RequireJS

--- public/game.js
+++ public/game.js
@@ -1,3 +1,5 @@
+import { binarySearch } from "./functions";
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -11,45 +13,59 @@ function setUpGrid() {
   let width = canvas.width;
   let height = canvas.height;
 
+  // Empty the Arrays
+  xCoords = [];
+  yCoords = [];
+
   ctx.fillStyle = "green";
   const numBins = 50; // numBins x numBins Grid
   let i;
-  for (i = 0; i < width; i += width / numBins) {
-    ctx.fillRect(i, 0, 1, height);
-    xCoords.push(i);
+  let j;
+
+  // Do not reset array on size rechange
+  if (filledCells.length == 0) {
+    for (i = 0; i < numBins; i++) {
+      filledCells.push([]);
+      for (j = 0; j < numBins; j++) {
+        filledCells[i].push(false);
+      }
+    }
+  }
+
+  for (i = 0; i < filledCells.length; i++) {
+    ctx.fillRect(i * (width / numBins), 0, 1, height);
+    xCoords.push(i * (width / numBins));
   }
   ctx.fillRect(width - 1, 0, 1, height);
   xCoords.push(width - 1);
 
-  for (i = 0; i < height; i += height / numBins) {
-    ctx.fillRect(0, i, width, 1);
-    yCoords.push(i);
+  for (i = 0; i < filledCells.length; i++) {
+    ctx.fillRect(0, i * (height / numBins), width, 1);
+    yCoords.push(i * (height / numBins));
   }
   ctx.fillRect(0, height - 1, width, 1);
   yCoords.push(height - 1);
-}
 
-// Binary Search for arrays
-// https://stackoverflow.com/questions/22697936/binary-search-in-javascript
-function binarySearch(arr, el) {
-  var m = 0;
-  var n = arr.length - 1;
-  while (m <= n) {
-    var k = (n + m) >> 1;
-    if (el > arr[k]) {
-      m = k + 1;
-    } else if (el < arr[k]) {
-      n = k - 1;
-    } else {
-      return k;
+  // Redraw Filled in Spaces
+  for (i = 0; i < filledCells.length; i++) {
+    for (j = 0; j < filledCells.length; j++) {
+      if (filledCells[i][j]) {
+        ctx.fillStyle = "black";
+        ctx.fillRect(
+          xCoords[j],
+          yCoords[i],
+          xCoords[1] - xCoords[0],
+          yCoords[1] - yCoords[0]
+        );
+      }
     }
   }
-  return -m - 1;
 }
 
 // Arrays to store coordinates
-const xCoords = [];
-const yCoords = [];
+let xCoords = [];
+let yCoords = [];
+let filledCells = [];
 
 setUpGrid();
 
@@ -64,8 +80,18 @@ canvas.addEventListener("click", function(event) {
   const y = event.pageY - 9;
 
   ctx.fillStyle = "black";
-  ctx.fillRect(x, y, 10, 10);
-  console.log(xCoords);
-  console.log(yCoords);
-  console.log(binarySearch(xCoords, x));
+
+  // Do binary search to get coords and for some reason subtract 2 to get correct index
+  let newX = -1 * binarySearch(xCoords, x) - 2;
+  let newY = -1 * binarySearch(yCoords, y) - 2;
+  ctx.fillRect(
+    xCoords[newX],
+    yCoords[newY],
+    xCoords[1] - xCoords[0],
+    yCoords[1] - yCoords[0]
+  );
+
+  // Mark the filled Cells
+  filledCells[newY][newX] = true;
+  console.log(filledCells);
 });
